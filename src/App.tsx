689b3b060e1ffc913d5494425d5213ea08d9b281
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-15 17:36:13 -0300

    Configuracao do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import styles from './App.module.css'
+
+function App() {
+  
+
+  return (
+    <div className={styles.container}>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/words.ts
@@ -0,0 +1,22 @@
+export type Challenge = {
+  id: number;
+  word: string;
+  tip: string;
+};
+
+export const WORDS: Challenge[] = [
+  { id: 1, word: "CSS", tip: "Linguagem de estilos" },
+  { id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web" },
+  { id: 3, word: "HTML", tip: "Linguagem de Marcação" },
+  {
+    id: 4,
+    word: "JAVASCRIPT",
+    tip: "Uma das linguagens de programação mais utilizadas no mundo",
+  },
+  {
+    id: 5,
+    word: "TYPESCRIPT",
+    tip: "Superset JavaScript que adiciona tipagem",
+  },
+  { id: 6, word: "TAILWIND CSS", tip: "Framework mais popular de CSS" },
+];
