6de212a9125f148b3e9128d5794cc5f9232cef51
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-18 16:47:59 -0300

    criando funcao para iniciar o jogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import Button from "./components/Button";
 import Guess from "./components/Guess";
@@ -5,30 +6,46 @@ import Header from "./components/Header";
 import Letter from "./components/Letter";
 import LettersUsed from "./components/LettersUsed";
 import Tip from "./components/Tip";
+import { WORDS, type Challenge } from "./utils/words";
 
 function App() {
+  const[challange, setChallenge] = useState<Challenge | null>(null)
+  const[attempts, setAttempts] = useState(0)
+  const[letter, setLetter] = useState('')
   function handleRestartGame() {
     alert("Jogo reiniciado");
   }
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randonWord = WORDS[index]
+    setChallenge(randonWord)
+    setAttempts(0)
+    setLetter('')
+  }
+  
 
+  useEffect(() => {
+    startGame();
+    
+  });
+
+  if(!challange) return
   return (
     <div className={styles.container}>
       <main>
-        <Header current={7} max={10} onRestart={handleRestartGame} />
-        <Tip tip="Linguagem de programacao para Front-End" />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
+        <Tip tip={challange?.tip} />
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="" />
-          <Letter value="" />
-          <Letter value="C" />
-          <Letter value="T" />
+          {challange.word.split('').map(() => <Letter/>)}
+
+          
         </div>
         <h4>Palpite</h4>
         <div>
-          <Guess autoFocus maxLength={1} placeholder="?"/>
-          <Button title="Confirmar"/>
+          <Guess autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
         </div>
-        <LettersUsed/>
+        <LettersUsed />
       </main>
     </div>
   );
